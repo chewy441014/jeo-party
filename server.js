d8d06edc481aa0d93003d3254da50dc8a81452a7
awgore99 <awgore99@gmail.com>
2022-11-04 13:37:01 -0500

    Added server.js to the repo

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
