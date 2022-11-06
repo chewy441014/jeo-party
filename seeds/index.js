3924afefb12875863cc72e16dab872368a4b8c02
awgore99 <awgore99@gmail.com>
2022-11-06 17:01:04 -0600

    Added basic seed script to be updated as need be later

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,16 @@
 const sequelize = require('../config/connection');
-const Question = require('../models/Question');
+const Question = require('../models/Questions');
+const Category = require('../models/Categories')
 const questionData = require('./jeopardy_qs.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await Question.bulkCreate(questionData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Category.bulkCreate(questionData, {
     individualHooks: true,
     returning: true,
   });
