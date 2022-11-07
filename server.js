d2801f8e7b5c3d45be39b437972b7f188b6ae817
Preston Hill <preston.leon.hill@gmail.com>
2022-11-07 14:40:44 -0600

    removed duplicate handlebars engine

--- server.js
+++ server.js
@@ -34,9 +34,6 @@ app.set('view engine', 'handlebars');
 
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.static(pubDirectPath));
 app.use(express.urlencoded({ extended: true }));
