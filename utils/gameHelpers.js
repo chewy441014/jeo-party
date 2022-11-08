30d5a0f82b56813325a1be19152ff96fdfe70478
awgore99 <awgore99@gmail.com>
2022-11-08 12:06:21 -0600

    Added submitAsnwer function to gameHelpers.js to update myTurn and the text field

--- utils/gameHelpers.js
+++ utils/gameHelpers.js
@@ -2,18 +2,33 @@
 
 const checkAnswer = (answerInput, correctAnswer) => {
     let answerBoolean = false;
-    if(answerInput.toLowerCase() === correctAnswer.toLowerCase()){
+    if (answerInput.toLowerCase() === correctAnswer.toLowerCase()) {
         answerBoolean = true;
     }
     return answerBoolean;
 };
 
-const addScore = ((value, currentScore) => {
+const addScore = (value, currentScore) => {
     currentScore = value + currentScore;
     return currentScore;
-});
+};
 
-const subtractScore = ((value, currentScore) => {
+const subtractScore = (value, currentScore) => {
     currentScore = currentScore - value;
     return currentScore;
-});
+};
+
+const submitAnswer = async function (event) {
+    // Need API route to get correctAnswer
+    if (checkAnswer(`#answerText`, correctAnswer)) {
+        document.getElementById('answerText').reset();
+        if (`#r${i}c${j}`) {
+            if (req.session.myTurn) {
+                req.session.myTurn = false;
+            }
+            else {
+                req.session.myTurn = true;
+            }
+        }
+    }
+};
