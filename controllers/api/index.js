918bef1edf9c992a7a0405089a00d63b61ad82ed
Katie Maddox <kmaddox2122@gmail.com>
2022-11-08 13:32:32 -0600

    updated all API and home routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
-const categoryRoutes = require('./category-routes.js');
 const gameRoutes = require('./game-routes.js');
 const questionRoutes = require('./question-routes.js');
 const scoreRoutes = require('./score-routes.js');
 const userRoutes = require('./user-routes.js');
 
-router.use('/categories', categoryRoutes);
 router.use('/games', gameRoutes);
 router.use('/questions', questionRoutes);
 router.use('/scores', scoreRoutes);
